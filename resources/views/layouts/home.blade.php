e2562ab0d68b9df050a676e717591a42272c528d
nenylukitasari <nenylukitasari@gmail.com>
2018-07-10 16:47:22 +0700

    Initial commit

---
+++ resources/views/layouts/home.blade.php
@@ -0,0 +1,52 @@
+<!DOCTYPE HTML>
+<html class="no-js" lang="">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Sufee Admin - HTML5 Admin Template</title>
+    <meta name="description" content="Sufee Admin - HTML5 Admin Template">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="apple-touch-icon" href="apple-icon.png">
+    <link rel="shortcut icon" href="favicon.ico">
+
+    <link rel="stylesheet" href="{{ URL::asset('css/normalize.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('css/font-awesome.min.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('css/themify-icons.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('css/flag-icon.min.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('css/cs-skin-elastic.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('css/scss/style.css') }}">
+
+    <link rel="stylesheet" href="{{ URL::asset('css/lib/vector-map/jqvmap.min.css') }}">
+
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800' rel='stylesheet' type='text/css'>
+
+    <!-- <script type="text/javascript" src="https://cdn.jsdelivr.net/html5shiv/3.7.3/html5shiv.min.js"></script> -->
+
+</head>
+
+<body>
+  @include('layouts.nav')
+  @yield('content')
+    <script src="{{ URL::asset('js/vendor/jquery-2.1.4.min.js') }}"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js"></script>
+    <script src="assets/js/plugins.js"></script>
+    <script src="assets/js/main.js"></script>
+
+
+    <script src="assets/js/lib/chart-js/Chart.bundle.js"></script>
+    <script src="assets/js/dashboard.js"></script>
+    <script src="assets/js/widgets.js"></script>
+    <script src="assets/js/lib/vector-map/jquery.vmap.js"></script>
+    <script src="assets/js/lib/vector-map/jquery.vmap.min.js"></script>
+    <script src="assets/js/lib/vector-map/jquery.vmap.sampledata.js"></script>
+    <script src="assets/js/lib/vector-map/country/jquery.vmap.world.js"></script>
+   
+  @yield('additional-script')
+
+
+</body>
+
+</html>
