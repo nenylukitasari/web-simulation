17ead68f1337f93a98cfbbf8612b6fe38950ba7c
nenylukitasari <nenylukitasari@gmail.com>
2018-07-15 20:55:18 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -10,36 +10,23 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 /*
 Route::get('/', function () {
     return view('realisasi.best-ott');
 });
 */
+
 Route::get('/', function () {
     return view('welcome');
 });
-Auth::routes();
-Route::get('/login', function () {
-    return redirect('/#login');
-});
-Route::get('/logout', 'Auth\LoginController@logout');
-Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/login', 'MainController@index');
+Route::post('/login/checklogin', 'MainController@checklogin');
+Route::get('admin', 'MainController@successlogin');
+Route::get('admin/logout', 'MainController@logout');
+
 //Route::get('/coba/{id}','OttController@index');//di get url
 Route::get('/coba','OttController@index');
 Route::post('/input','OttController@input');
 Route::get('/form','OttController@form');
 
-//Auth::routes();
-
-//Route::get('/home', 'HomeController@index')->name('home');
