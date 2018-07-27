ed0ee2c93811a900caeb8e5aa62f67ffedcbd586
nenylukitasari <nenylukitasari@gmail.com>
2018-07-27 10:03:07 +0700

    updated-web

--- routes/web.php
+++ routes/web.php
@@ -49,11 +49,6 @@ Route::post('/best-ott/inputsales','OttController@inputsales');
 Route::get('/form','OttController@form');
 Route::post('/input','OttController@input');
 
-
-
-	//Route::get('/admin/realisasi/best-ott/form','OttController@form');
-	//Route::post('/input','OttController@input');
-
 Route::get('/login', 'MainController@index');
 Route::post('/login/checklogin', 'MainController@checklogin');
 Route::get('admin/logout', 'MainController@logout');
@@ -62,84 +57,53 @@ Route::get('admin/logout', 'MainController@logout');
 Route::group(['middleware' => 'authenticated'], function() {
 	Route::get('/admin', 'MainController@successlogin');
 
+	Route::post('/input_minipack','AddonController@input_minipack');
+	Route::post('/realisasi_minipack','AddonController@realisasi_minipack');
+	Route::post('/target_minipack','AddonController@target_minipack');
+
+	Route::post('/input_stb','AddonController@input_stb');
+	Route::post('/realisasi_stb','AddonController@realisasi_stb');
+	Route::post('/target_stb','AddonController@target_stb');
+
+	Route::post('/input_telepon','AddonController@input_telepon');
+	Route::post('/realisasi_telepon','AddonController@realisasi_telepon');
+	Route::post('/target_telepon','AddonController@target_telepon');
+
+	Route::post('/input_upspeed','AddonController@input_upspeed');
+	Route::post('/realisasi_upspeed','AddonController@realisasi_upspeed');
+	Route::post('/target_upspeed','AddonController@target_upspeed');
 
 });
 
-
-//USER - simulasi
-Route::get('/index','AddonController@index');
-Route::post('/searchminipack','AddonController@postminipack');
-Route::post('/input_minipack','AddonController@input_minipack');
-Route::get('/minipack','AddonController@getminipack');
-
-
-Route::get('/simulasi/best-ott/catchplay','OttController@getcatchplay');
-Route::get('/simulasi/best-ott/iflix','IflixController@index');
-Route::get('/simulasi/best-ott/hooq','HooqController@index');
-Route::get('/simulasi/best-ott/movin','MovinController@index');
-
-Route::get('/simulasi/best-addon/minipack','AddonController@getcatchplay');
-Route::get('/formneny','AddonController@form');
-
-Route::get('/simulasi/best-addon/STBtambahan', 'STBController@index');
-Route::get('/simulasi/best-addon/teleponmania', 'TeleponManiaController@index');
-Route::get('/simulasi/best-addon/upgradespeed', 'UpgradeSpeedController@index');
-
-
-
-//NENY
+//best-addon
 Route::get('/best-addon','AddonController@index');
 Route::post('/searchbestaddon','AddonController@searchbestaddon');
 
 Route::get('/best-addon/minipack/input','AddonController@getminipack_input');
 Route::post('/searchinputminipack','AddonController@postminipack_input');
-
 Route::get('/best-addon/stb/input','AddonController@getstb_input');
 Route::post('/searchinputstb','AddonController@poststb_input');
-
 Route::get('/best-addon/telepon/input','AddonController@gettelepon_input');
 Route::post('/searchinputtelepon','AddonController@posttelepon_input');
-
 Route::get('/best-addon/upspeed/input','AddonController@getupspeed_input');
 Route::post('/searchinputupspeed','AddonController@postupspeed_input');
 
 Route::get('/best-addon/minipack/realisasi','AddonController@getminipack_realisasi');
 Route::post('/searchrealisasiminipack','AddonController@postminipack_realisasi');
-
 Route::get('/best-addon/stb/realisasi','AddonController@getstb_realisasi');
 Route::post('/searchrealisasistb','AddonController@poststb_realisasi');
-
 Route::get('/best-addon/telepon/realisasi','AddonController@gettelepon_realisasi');
 Route::post('/searchrealisasitelepon','AddonController@posttelepon_realisasi');
-
 Route::get('/best-addon/upspeed/realisasi','AddonController@getupspeed_realisasi');
 Route::post('/searchrealisasiupspeed','AddonController@postupspeed_realisasi');
 
 Route::get('/best-addon/minipack/target','AddonController@getminipack_target');
 Route::post('/searchtargetminipack','AddonController@postminipack_target');
-
 Route::get('/best-addon/stb/target','AddonController@getstb_target');
 Route::post('/searchtargetstb','AddonController@poststb_target');
-
 Route::get('/best-addon/telepon/target','AddonController@gettelepon_target');
 Route::post('/searchtargettelepon','AddonController@posttelepon_target');
-
 Route::get('/best-addon/upspeed/target','AddonController@getupspeed_target');
 Route::post('/searchtargetupspeed','AddonController@postupspeed_target');
 
-//admin
-Route::post('/input_minipack','AddonController@input_minipack');
-Route::post('/realisasi_minipack','AddonController@realisasi_minipack');
-Route::post('/target_minipack','AddonController@target_minipack');
 
-Route::post('/input_stb','AddonController@input_stb');
-Route::post('/realisasi_stb','AddonController@realisasi_stb');
-Route::post('/target_stb','AddonController@target_stb');
-
-Route::post('/input_telepon','AddonController@input_telepon');
-Route::post('/realisasi_telepon','AddonController@realisasi_telepon');
-Route::post('/target_telepon','AddonController@target_telepon');
-
-Route::post('/input_upspeed','AddonController@input_upspeed');
-Route::post('/realisasi_upspeed','AddonController@realisasi_upspeed');
-Route::post('/target_upspeed','AddonController@target_upspeed');
